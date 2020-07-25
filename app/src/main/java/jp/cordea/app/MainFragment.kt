3cc2dd428f59b2fbf8a4a225429e954e02e35b4c
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-25 12:37:28 +0900

    rename

--- app/src/main/java/jp/cordea/app/MainFragment.kt
+++ app/src/main/java/jp/cordea/app/MainFragment.kt
@@ -3,11 +3,11 @@ package jp.cordea.app
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import jp.cordea.app.databinding.FragmentFirstBinding
+import jp.cordea.app.databinding.MainFragmentBinding
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
+class MainFragment : Fragment(R.layout.main_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentFirstBinding.bind(view)
+        val binding = MainFragmentBinding.bind(view)
     }
 }
