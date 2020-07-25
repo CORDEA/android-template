f453379b8ccfa4c31f651124eee858b4d834b25c
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-25 12:35:42 +0900

    use data binding

--- app/src/main/java/jp/cordea/app/FirstFragment.kt
+++ app/src/main/java/jp/cordea/app/FirstFragment.kt
@@ -1,21 +1,13 @@
 package jp.cordea.app
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import jp.cordea.app.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+class FirstFragment : Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentFirstBinding.bind(view)
     }
 }
