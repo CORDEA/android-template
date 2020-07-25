32d4e80e410d8d497f54aa5a58e0d9159621c47a
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-25 12:46:23 +0900

    enable view binding

--- app/src/main/java/jp/cordea/app/MainActivity.kt
+++ app/src/main/java/jp/cordea/app/MainActivity.kt
@@ -2,11 +2,13 @@ package jp.cordea.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import jp.cordea.app.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 }
